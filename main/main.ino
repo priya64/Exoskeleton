2e0a8da34b622307753aa1a34f673e8661c8c1a5
Andrea MC <andreita.mart@hotmail.com>
2020-01-31 19:49:53 -0500

    Add servo movement code to infinite loop

--- main/main.ino
+++ main/main.ino
@@ -4,22 +4,37 @@
  * 
  * This is the code for the exoskeleton project.
  * It must rotate in both directions to account for a finger unfolding and folding,
- * as well as only do so if the user chooses to (reading from sensor).
+ * as well as only do so if the user chooses to by flexing (reading from a muscle sensor).
  * 
  * Author: Andrea Martinez Chung
  *********************************************************************/
 #include <Servo.h>
 
 #define servo_pin 13
+#define sensor_pin A0
 
 Servo myservo;       //Declare servo object
-int pos = 0;        //Variable to store the servo position
+int pos = 0;         //Variable to store the servo position
+int sensor_value;     //Varible to store value at sensor
 
 void setup() {
   myservo.attach(servo_pin);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  sensor_value = analogRead(sensor_pin);
+  
+  if(sensor_value > 100){                //move finger if user flexes
+    for(pos = 0; pos <= 90; pos++){      //rotate servo from 0 degrees to 90 degrees
+      myservo.write(pos);
+      delay(15);
+    }
+    
+    while(sensor_value > 10){}          //while the user is still flexing, don't do anything
+    
+    for(pos = 90; pos >=0; pos--){      //rotate servo from 90 degrees to 0 degrees
+      myservo.write(pos);
+      delay(15);
+    }
+  } 
 }
