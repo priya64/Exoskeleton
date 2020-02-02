608879316fef763a96ae278d7d56e5ef007eacdc
priya64 <39603155+priya64@users.noreply.github.com>
2020-02-02 22:11:54 +0200

    Update Motor_code.ino
    Added potentiometer as substitute for EMG sensor code

--- Motor_code.ino
+++ Motor_code.ino
@@ -1,17 +1,23 @@
 #include <Servo.h>
+//#include <SoftwareSerial.h>
 
 Servo servo;
 
 int pin = 9; 
 int pos = 0; //position is influenced by sensor
 
-
+int potpin = A0;
+double pot_num = 0;
+double calc = 0;
 
 void setup() {
 
   //initialise motors
   
   servo.attach(pin);
+
+  //read from potentiometer
+ Serial.begin(9600);
 }
 
 void loop() {
@@ -19,13 +25,20 @@ void loop() {
   //take in signal from motor
   //write which direction to move in
 
-   for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     servo.write(pos);
     delay(15);                     // waits 15ms for the servo to reach the position
-  }
+    }
   for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
     servo.write(pos);            // tell servo to go to position in variable 'pos'
     delay(15);
-  }
+    }
+    
+  delay(500);
+  
+  pot_num = analogRead(potpin);
+  calc = (pot_num/1023.0)*10000;
+  Serial.println(calc);
+  delay(1000);
 }
